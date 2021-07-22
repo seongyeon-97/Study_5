d4c353ae69c2092774409334f96d77898e05ad4f
seongyeon-97 <creative9711@naver.com>
2021-07-22 12:27:38 +0900

    07/22 class

--- src/com/iu/s4/Test.java
+++ src/com/iu/s4/Test.java
@@ -3,6 +3,13 @@ package com.iu.s4;
 public class Test {
 	
 	int num = 10;
-	final int num2 = 8;
+	final int num2;
 	
+	public Test() {
+		num2 = 10;
+	}
+	
+	public Test(int num2) {
+		this.num2 = num2;
+	}
 }
